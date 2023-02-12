2a82713263b4d66f167634c2f77a6760b1db4ea5
abdullah <abdullahsaad8701@gmail.com>
2023-02-12 23:24:47 +0500

    pushing changes

--- test.java
+++ test.java
@@ -9,7 +9,7 @@ public class test {
 	public static void main(String[] args) {
 		//code to print Unicode characters
 		int charPerLine=0;
-		for (int 1 = 33; 1 < 256; i++, charPerLine++)
+		for (int 1 = 20; 1 < 400; i++, charPerLine++)
 		{
 		System.out.print((char){+((charPerLine%10--0)?"\n*:"\t"));
 		System. out .print ();
